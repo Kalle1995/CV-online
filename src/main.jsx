70f29a2c4792b7157f01f0fe680f951dfc109899
Khaled Heteni <hetiny.khaled@gmail.com>
2025-05-20 20:51:55 +0200

    hela main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, HashRouter } from 'react-router-dom'; // 👈 importera BrowserRouter
-import { HashRouter as Router } from "react-router-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 
-import './index.css'
-import App from './App.jsx'
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/CV-online"> {/* 👈 lägg till basename här */}
+    <HashRouter>
       <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+    </HashRouter>
+  </StrictMode>
+);
