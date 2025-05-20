8465a420a8d73c07599b0e9dd3739dd23a8206b8
Khaled Heteni <hetiny.khaled@gmail.com>
2025-05-20 19:13:06 +0200

    la till base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/CV-online/',   // Lägg till den här raden med ditt repo-namn
   server: {
     port: 3000,
   }
