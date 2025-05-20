34e8e0ae7a667604f9d73c63733ef47f0fce4fd5
Khaled Heteni <hetiny.khaled@gmail.com>
2025-05-20 20:35:30 +0200

    pdf länkar

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'; // 👈 importera BrowserRouter
+import { HashRouter as Router } from "react-router-dom";
+
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/CV-online"> {/* 👈 lägg till basename här */}
+    <Router basename="/CV-online"> {/* 👈 lägg till basename här */}
       <App />
-    </BrowserRouter>
+    </Router>
   </StrictMode>,
 )
